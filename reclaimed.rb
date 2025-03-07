3c4220fb0b97ba9d4de9df2dc359e17d0668ebe0
Taylor Wilsdon <tbarrettwilsdon@gmail.com>
2025-03-07 11:40:46 -0500

    0.2.2.

--- reclaimed.rb
+++ reclaimed.rb
@@ -13,17 +13,17 @@ class Reclaimed < Formula
   # Runtime dependencies only - no build dependencies needed
   resource "click" do
     url "https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b/click-8.1.8.tar.gz"
-    sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
+    sha256 "65030eae9b743f6f45ba75da103abc7a41dd95c856276fc8dcb38c99ddf17a19"
   end
 
   resource "rich" do
     url "https://files.pythonhosted.org/packages/ab/3a/0316b28d0761c6734d6bc14e770d85506c986c85ffb239e688eeaab2c2bc/rich-13.9.4.tar.gz"
-    sha256 "439594978a49a09530cff7ebc4b5c7103ef57baf48d5ea3184f21d9a2befa098"
+    sha256 "65030eae9b743f6f45ba75da103abc7a41dd95c856276fc8dcb38c99ddf17a19"
   end
 
   resource "textual" do
     url "https://files.pythonhosted.org/packages/41/62/4af4689dd971ed4fb3215467624016d53550bff1df9ca02e7625eec07f8b/textual-2.1.2.tar.gz"
-    sha256 "aae3f9fde00c7440be00e3c3ac189e02d014f5298afdc32132f93480f9e09146"
+    sha256 "65030eae9b743f6f45ba75da103abc7a41dd95c856276fc8dcb38c99ddf17a19"
   end
 
   def install
