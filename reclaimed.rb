ccc799bcd8c5104b51a7dbea7a9a168ae3a8993a
Taylor Wilsdon <tbarrettwilsdon@gmail.com>
2025-03-07 11:15:21 -0500

    deploy reclaimed v0.2.0

--- reclaimed.rb
+++ reclaimed.rb
@@ -3,30 +3,14 @@ class Reclaimed < Formula
 
   desc "A powerful disk usage analyzer with iCloud support"
   homepage "https://github.com/taylorwilsdon/reclaimed"
-  version "0.1.7"
+  version "0.2.0"
   url "https://github.com/taylorwilsdon/reclaimed/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "4831097bbffd108dcc088ddd6a3aa6a9c6d5c01ffbe2cccfc98dbec755f7cb70"
+  sha256 "b1c19c57fb53c6add218efac6c00c5f74131c7dee384274f059f16d0902c07f2"
   license "MIT"
 
   depends_on "python@3.11"
   
-  # Build dependencies
-  resource "hatchling" do
-    url "https://files.pythonhosted.org/packages/8f/8a/cc1debe3514da292094f1c3a700e4ca25442489731ef7c0814358816bb03/hatchling-1.27.0.tar.gz"
-    sha256 "971c296d9819abb3811112fc52c7a9751c8d381898f36533bb16f9791e941fd6"
-  end
-
-  resource "hatch-vcs" do
-    url "https://files.pythonhosted.org/packages/f5/c9/54bb4fa27b4e4a014ef3bb17710cdf692b3aa2cbc7953da885f1bf7e06ea/hatch_vcs-0.4.0.tar.gz"
-    sha256 "093810748fe01db0d451fabcf2c1ac2688caefd232d4ede967090b1c1b07d9f7"
-  end
-
-  resource "setuptools-scm" do
-    url "https://files.pythonhosted.org/packages/4b/bd/c5d16dd95900567e09744af92119da7abc5f447320d53ec1d9415ec30263/setuptools_scm-8.2.0.tar.gz"
-    sha256 "a18396a1bc0219c974d1a74612b11f9dce0d5bd8b1dc55c65f6ac7fd609e8c28"
-  end
-
-  # Runtime dependencies
+  # Runtime dependencies only - no build dependencies needed
   resource "click" do
     url "https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b/click-8.1.8.tar.gz"
     sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d16096a"
@@ -43,8 +27,6 @@ class Reclaimed < Formula
   end
 
   def install
-    # Set static version for build since we don't have git metadata in the tarball
-    ENV["SETUPTOOLS_SCM_PRETEND_VERSION"] = version
     virtualenv_install_with_resources
   end
 
